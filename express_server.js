a3c234025e38db3013f22e4f49afd3681ad90912
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-01-25 21:25:19 -0700

    added cookies feature

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const cookie = require('cookie-parser');
 app.set('view engine', 'ejs');
+app.use(cookie());
 
 const rndmNum = function () {
 	let rndmStr = (Math.random() + 1).toString(36).substring(7);
@@ -16,9 +18,20 @@ const urlDatabase = {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/login', (req, res) => {
+	res.cookie('username', req.body.username);
+	res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+	res.clearCookie('username', req.body.username);
+	res.redirect('/urls');
+});
+
 app.post('/urls', (req, res) => {
 	const shortURL = rndmNum();
 	urlDatabase[shortURL] = req.body.longURL;
+
 	res.redirect(`/urls/${shortURL}`);
 });
 
@@ -27,22 +40,37 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 	res.redirect(`/urls`);
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+	const shortURL = req.params.shortURL;
+	const longURL = req.body.longURL;
+	urlDatabase[shortURL] = longURL;
+
+	res.redirect('/urls');
+});
+
 app.get('/u/:shortURL', (req, res) => {
 	const longURL = urlDatabase[req.params.shortURL];
 	res.redirect(longURL);
 });
 
 app.get('/urls/new', (req, res) => {
-	res.render('urls_new');
+	const templateVars = {
+		username: req.cookies['username'],
+	};
+	res.render('urls_new', templateVars);
 });
 
 app.get('/urls', (req, res) => {
-	const templateVars = { urls: urlDatabase };
+	const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
 	res.render('urls_index', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-	const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+	const templateVars = {
+		shortURL: req.params.shortURL,
+		longURL: urlDatabase[req.params.shortURL],
+		username: req.cookies['username'],
+	};
 	res.render('urls_show', templateVars);
 });
 
